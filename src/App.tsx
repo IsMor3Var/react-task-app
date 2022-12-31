9541f625fa1ad403a6e0f72ae6c8162740548ea3
IsMor3Var <lmoreno9943@gmail.com>
2022-12-31 18:16:44 -0500

    Create hook redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useAppSelector } from './app/hooks';
 
 function App(): JSX.Element{
+
+  const tasksState = useAppSelector( state => state.tasks )
+
   return (
     <h1>Hello World</h1>
   );
